b22c1def00b5e8a8ccc8f810d52f29d36e5cf6ab
Mohammad Rafid Ul Islam <rai009@Mohammads-MacBook-Air.local>
2021-05-21 12:08:15 -0700

    new embedding a dataset

--- code2vec.py
+++ code2vec.py
@@ -1,23 +1,36 @@
 from gensim import models
 import os
+import json
 
-# ast_folders = ['../SE/train-asts/', '../SE/eval-asts/', '../corpus/']
+ast_folders = ['../SE/train-asts/', '../SE/eval-asts/', '../py150/']
 
-# corpus = []
+corpus = []
+def to_string_list(code_list):
+    return [str(c) for c in code_list]
 
-# for folder in ast_folders:
-#     directory = os.fsencode(folder)
-#     for file in os.listdir(directory):
-#         ast_file = os.fsdecode(file)
-#         if os.path.isdir(folder + ast_file):
-#             continue
-#         f = open(folder + ast_file, 'r')
-#         splits = f.read().split()
-#         corpus.append(splits)
-#         f.close()
+def data_to_json(filename):
+    i = 1
+    f = open(filename, 'r')
+    while True:
+        line = f.readline()
+        if not line:
+            break
+        line = eval(line.strip())
+        corpus.append(to_string_list(line))
 
-# code2vec = models.FastText(corpus, vector_size=128, window=5, min_count=5, workers=4,sg=1)
-# code2vec.save('code2vec.model')
+    
+for folder in ast_folders:
+    directory = os.fsencode(folder)
+    for file in os.listdir(directory):
+        ast_file = os.fsdecode(file)
+        if not ast_file.endswith(".json") or os.path.isdir(folder + ast_file):
+            continue
+        data_to_json(folder + ast_file)
 
-code2vec = models.FastText.load('code2vec.model')
-print(code2vec.wv.most_similar('elif', topn=10))
+
+code2vec = models.FastText(corpus, vector_size=128, window=5, min_count=5, workers=4,sg=1)
+code2vec.save('code2vec.model')
+
+# code2vec = models.FastText.load('code2vec.model')
+# print(code2vec.wv.most_similar('elif', topn=10))
+# data_to_json('../py150/python100k_train.json')
--- prepare_data.py
+++ prepare_data.py
@@ -11,6 +11,133 @@ directory_name = {'eval': '../SE/eval/', 'train': '../SE/train/'}
 output_folder = {'eval': '../SE/eval-asts/', 'train': '../SE/train-asts/'}
 csv_name = {'eval': '../SE/eval', 'train': '../SE/train'}
 
+
+def read_file_to_string(filename):
+    f = open(filename, 'rt')
+    s = f.read()
+    f.close()
+    return s
+
+def parse_file(filename):
+    global c, d
+    tree = ast.parse(read_file_to_string(filename), filename)
+    
+    json_tree = []
+    def gen_identifier(identifier, node_type = 'identifier'):
+        pos = len(json_tree)
+        json_node = {}
+        json_tree.append(json_node)
+        json_node['type'] = node_type
+        json_node['value'] = identifier
+        return pos
+    
+    def traverse_list(l, node_type = 'list'):
+        pos = len(json_tree)
+        json_node = {}
+        json_tree.append(json_node)
+        json_node['type'] = node_type
+        children = []
+        for item in l:
+            children.append(traverse(item))
+        if (len(children) != 0):
+            json_node['children'] = children
+        return pos
+        
+    def traverse(node):
+        pos = len(json_tree)
+        json_node = {}
+        json_tree.append(json_node)
+        json_node['type'] = type(node).__name__
+        children = []
+        if isinstance(node, ast.Name):
+            json_node['value'] = node.id
+        elif isinstance(node, ast.Num):
+            json_node['value'] = str(node.n)
+        elif isinstance(node, ast.Str):
+            json_node['value'] = str(node.s)
+        elif isinstance(node, ast.alias):
+            json_node['value'] = str(node.name)
+            if node.asname:
+                children.append(gen_identifier(node.asname))
+        elif isinstance(node, ast.FunctionDef):
+            json_node['value'] = str(node.name)
+        elif isinstance(node, ast.ClassDef):
+            json_node['value'] = str(node.name)
+        elif isinstance(node, ast.ImportFrom):
+            if node.module:
+                json_node['value'] = str(node.module)
+        elif isinstance(node, ast.Global):
+            for n in node.names:
+                children.append(gen_identifier(n))
+        elif isinstance(node, ast.keyword):
+            json_node['value'] = str(node.arg)
+        
+
+        # Process children.
+        if isinstance(node, ast.For):
+            children.append(traverse(node.target))
+            children.append(traverse(node.iter))
+            children.append(traverse_list(node.body, 'body'))
+            if node.orelse:
+                children.append(traverse_list(node.orelse, 'orelse'))
+        elif isinstance(node, ast.If) or isinstance(node, ast.While):
+            children.append(traverse(node.test))
+            children.append(traverse_list(node.body, 'body'))
+            if node.orelse:
+                children.append(traverse_list(node.orelse, 'orelse'))
+        # elif isinstance(node, ast.With):
+        #     children.append(traverse(node.context_expr))
+        #     if node.optional_vars:
+        #         children.append(traverse(node.optional_vars))
+            children.append(traverse_list(node.body, 'body'))
+        elif isinstance(node, ast.Try):
+            children.append(traverse_list(node.body, 'body'))
+            children.append(traverse_list(node.handlers, 'handlers'))
+            if node.orelse:
+                children.append(traverse_list(node.orelse, 'orelse'))
+        # elif isinstance(node, ast.ExceptHandler):
+        #     children.append(traverse_list(node.body, 'body'))
+        #     children.append(traverse_list(node., 'finalbody'))
+        elif isinstance(node, ast.arguments):
+            children.append(traverse_list(node.args, 'args'))
+            children.append(traverse_list(node.defaults, 'defaults'))
+            if node.vararg:
+                children.append(gen_identifier(node.vararg, 'vararg'))
+            if node.kwarg:
+                children.append(gen_identifier(node.kwarg, 'kwarg'))
+        elif isinstance(node, ast.ExceptHandler):
+            if node.type:
+                children.append(traverse_list([node.type], 'type'))
+            if node.name:
+                children.append(traverse_list([node.name], 'name'))
+            children.append(traverse_list(node.body, 'body'))
+        elif isinstance(node, ast.ClassDef):
+            children.append(traverse_list(node.bases, 'bases'))
+            children.append(traverse_list(node.body, 'body'))
+            children.append(traverse_list(node.decorator_list, 'decorator_list'))
+        elif isinstance(node, ast.FunctionDef):
+            children.append(traverse(node.args))
+            children.append(traverse_list(node.body, 'body'))
+            children.append(traverse_list(node.decorator_list, 'decorator_list'))
+        else:
+            # Default handling: iterate over children.
+            for child in ast.iter_child_nodes(node):
+                if isinstance(child, ast.expr_context) or isinstance(child, ast.operator) or isinstance(child, ast.boolop) or isinstance(child, ast.unaryop) or isinstance(child, ast.cmpop):
+                    # Directly include expr_context, and operators into the type instead of creating a child.
+                    json_node['type'] = json_node['type'] + type(child).__name__
+                else:
+                    children.append(traverse(child))
+                
+        if isinstance(node, ast.Attribute):
+            children.append(gen_identifier(node.attr, 'attr'))
+                
+        if (len(children) != 0):
+            json_node['children'] = children
+        return pos
+    
+    traverse(tree)
+    return json.dumps(json_tree, separators=(',', ':'), ensure_ascii=False)
+
 table = {}
 for key in directory_name:
     if not os.path.isdir(output_folder[key]):
@@ -33,12 +160,13 @@ for key in directory_name:
                 table[splits[0]]["input"] = filename
             else:
                 table[splits[0]]["input"] = filename
-            f = open(directory_name[key] + filename, 'r')
-            tree = ast2json.ast2json(ast.parse(f.read()))
+            # f = open(directory_name[key] + filename, 'r')
+            # tree = ast2json.ast2json(ast.parse(f.read()))
             out = open(output_folder[key] + splits[0] + '.json', 'w')
             table[splits[0]]['ast'] = splits[0] + '.json'
-            out.write(json.dumps(tree))
-            f.close()
+            out.write(parse_file(directory_name[key] + filename))
+            out.close()
+            # f.close()
     data = {'input': [], 'label': [], 'ast': []}
 
     for file_key in table:
